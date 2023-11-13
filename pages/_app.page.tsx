88b6b133a6c9bf178148e67e1438dd04cd0a30b3
HanSeongLee <jkhanseong@gmail.com>
2023-11-13 18:52:56 +0900

    feat: add Rubik font

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Rubik } from '@next/font/google';
+
+const rubikFont = Rubik({
+  weight: ['300', '400', '500'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+    <style jsx global>{`
+      :root {
+        --rubik-font: ${rubikFont.style.fontFamily};
+      }
+    `}</style>
+    <Component {...pageProps} />
+  </>;
 }
 
 export default MyApp
